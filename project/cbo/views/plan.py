cb4f5cf8e00559ba5cd59833d153cab6e5ac0915
Rafael <rafael.pinheiro@dmslog.com>
2025-01-20 00:17:45 -0300

    refact: register layout

--- project/cbo/views/plan.py
+++ project/cbo/views/plan.py
@@ -1,20 +1,16 @@
 from django.views import View
-from django.http import JsonResponse
-from django.shortcuts import render
-from django.urls import reverse
-from django.utils.decorators import method_decorator
+from django.shortcuts import render, redirect
 from ..models import Plan
 
 
 class PlanView(View):
     def get(self, request):
-        order = ['Plano Essencial', 'Plano Essencial +', 'Plano Codificador/Faturista']
-        plans = Plan.objects.all().order_by('name')
-        plans_ordered = sorted(plans, key=lambda x: order.index(x.name) if x.name in order else len(order))
-
-        return render(request, 'create/select_plan.html', {'plans': plans_ordered})
+        plans = Plan.objects.all().order_by('-update_at')
+        return render(request, 'create/select_plan.html', {'plans': plans})
 
     def post(self, request):
-        selected_plan_id = request.POST.get('selected_plan_id')
-        redirect_url = reverse('register', kwargs={'selected_plan': selected_plan_id})
-        return JsonResponse({'redirect_url': redirect_url})
+        selected_plan_id = request.POST.get('plan_id')
+        if selected_plan_id:
+            return redirect('register', selected_plan=selected_plan_id)
+        else:
+            return render(request, 'create/select_plan.html', {'error': 'Plano não selecionado'})
